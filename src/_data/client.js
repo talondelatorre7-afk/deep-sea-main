88730d7497a5b96bed6ffd915bdde79cdfd8f7ee
Talon De La Torre <tddmedia1@gmail.com>
2026-04-17 16:33:20 -0700

    Added pricing page.

--- src/_data/client.js
+++ src/_data/client.js
@@ -5,7 +5,7 @@ module.exports = {
     phoneFormatted: "(909) 919-0912",
     socials: {
         facebook: "https://www.facebook.com/log00001",
-        instagram: "https://www.instagram.com/tdspots",
+        instagram: "https://www.instagram.com/deepseawebdesign",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
     domain: "https://www.example.com",

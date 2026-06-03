97f9faa9a07807bc79f182d4144d8c38c8e723c2
Talon De La Torre <tddmedia1@gmail.com>
2026-06-03 15:51:55 -0700

    updated to new number, added privacy and terms added sms opt-in in contact page

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: "Deep Sea Digital",
     email: "talondelatorre7@gmail.com",
-    phoneForTel: "909-919-0912",
-    phoneFormatted: "(909) 919-0912",
+    phoneForTel: "951-592-8666",
+    phoneFormatted: "(951) 592-8666",
     socials: {
         facebook: "https://www.facebook.com/log00001",
         instagram: "https://www.instagram.com/deepseawebdesign",
